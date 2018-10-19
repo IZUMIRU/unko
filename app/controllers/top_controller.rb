9fdfa18d71461082e2506e1d30fd6f004ed16f19
IZUMIRU <izumiruryousuke@gmail.com>
2018-10-19 21:39:11 +0900

    feat: 不要な変数の削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,19 +1,4 @@
 class TopController < ApplicationController
   def index
-    case rand(6)
-      when 0
-        @audio = '綿引うんこ.m4a'
-      when 1
-        @audio = '森泉うんこ.m4a'
-      when 2
-        @audio = '押田うんこ.m4a'
-      when 3
-        @audio = '根岸うんこ.m4a'
-      when 4
-        @audio = '高久うんこ.m4a'
-      when 5
-        @audio = '三宅うんこ.m4a'
-      else 0
-    end
   end
 end
